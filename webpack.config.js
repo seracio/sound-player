6135b2d82178f430102908c168735331cf4a6b8c
nicolasmondon <nicolasmondon@gmail.com>
2016-12-30 00:16:49 +0100

    webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,22 @@
 const path = require('path');
-const webpack = require('webpack');
 
-module.exports = options => {
-
-  return {
-    entry: {
-      'index': path.resolve('src/index.js'),
-    },
-    output: {
-      filename: '[name].js',
-      path: path.resolve('dist'),
-      publicPath: '/dist/',
-    },
-    externals: {
-      'jsmediatags': 'jsmediatags'
-    },
-    module: {
-      loaders: [{
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      }],
-    },
-  };
-};
+module.exports = () => ({
+  entry: {
+    'index': path.resolve('src/index.js'),
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve('dist'),
+    publicPath: '/dist/',
+  },
+  externals: {
+    'jsmediatags': 'jsmediatags'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+    }],
+  },
+});
