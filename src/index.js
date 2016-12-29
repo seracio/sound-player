1c6fe2d4e5aebdaf0b8f73ea94fc8ad45fbf4245
nicolasmondon <nicolasmondon@gmail.com>
2016-12-29 20:34:08 +0100

    use xstream

--- src/index.js
+++ src/index.js
@@ -5,41 +5,48 @@ import * as d3 from 'd3-selection';
 import xs from 'xstream';
 import jsmediatags from 'jsmediatags';
 
-(async function main() {
+// load the file as an audioBuffer
+// https://www.html5rocks.com/en/tutorials/webaudio/intro/
+const context = new AudioContext();
+const filename = 'data/testpodcastfigtech-v2.mp3';
 
-  // load the file as an audioBuffer
-  // https://www.html5rocks.com/en/tutorials/webaudio/intro/
-  const context = new AudioContext();
+const blob$ = xs
+  .fromPromise(fetch(filename).then(res => res.blob()))
+  .remember();
 
-  const filename = 'data/testpodcastfigtech-v2.mp3';
+const audioBuffer$ = xs
+  .fromPromise(
+    fetch(filename)
+      .then(res => res.arrayBuffer())
+      .then(buffer => context.decodeAudioData(buffer))
+  )
+  .remember();
 
-  let myBlob = await fetch(filename)
-    .then(res => res.blob());
+const source$ = audioBuffer$
+  .map(audioBuffer => {
+    const source = context.createBufferSource();
+    source.buffer = audioBuffer;
+    source.connect(context.destination);
+    return source;
+  })
+  .remember();
 
-  const myAudioBuffer = await fetch(filename)
-    .then(res => res.arrayBuffer())
-    .then(buff => context.decodeAudioData(buff));
+const tag$ = blob$
+  .map(blob => xs.fromPromise(new Promise(res => jsmediatags.read(blob, {onSuccess: res}))))
+  .flatten()
+  .remember();
 
-  // play sound
-  console.log(myAudioBuffer);
-  const source = context.createBufferSource();
-  source.buffer = myAudioBuffer;
-  source.connect(context.destination);
-  //source.start(0);
+tag$.addListener({
+  next: tag => {
+    // https://github.com/43081j/id3/issues/12
+    // https://github.com/43081j/id3/pull/21
+    const arrayBufferView = new Uint8Array(tag.tags.picture.data);
+    const blob = new Blob([arrayBufferView], {type: tag.tags.picture.format});
+    const imageURL = URL.createObjectURL(blob);
+    d3.select('body').append('img').attr('src', imageURL);
+  }
+});
 
-  jsmediatags.read(myBlob, {
-    onSuccess: function(tag) {
-      console.log(tag);
-      // https://github.com/43081j/id3/issues/12
-      // https://github.com/43081j/id3/pull/21
-      const arrayBufferView = new Uint8Array(tag.tags.picture.data);
-      const blob = new Blob([arrayBufferView], {type: tag.tags.picture.format});
-      const imageURL = URL.createObjectURL(blob);
-      d3.select('body').append('img').attr('src', imageURL);
-    },
-    onError: function(error) {
-      console.log(error);
-    }
-  });
-
-}());
+source$.addListener({
+  next: source => source.start(0)
+});
